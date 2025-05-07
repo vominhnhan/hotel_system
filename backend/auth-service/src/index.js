8ba26452a9bf74a55bcd448c07c9daaf8379037e
Giahao <Admin@LAPTOP-1JGLQK4M>
2025-05-07 12:37:22 +0700

    auth

--- backend/auth-service/src/index.js
+++ backend/auth-service/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.router.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 
