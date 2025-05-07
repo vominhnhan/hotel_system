a34142748d12a9d37814332344b136abd61c50ec
Giahao <Admin@LAPTOP-1JGLQK4M>
2025-05-07 15:12:33 +0700

    service

--- backend/service-management-service/src/index.js
+++ backend/service-management-service/src/index.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import serviceRoutes from './routes/serviceRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/services', serviceRoutes);
 app.use(errorHandler);
 
